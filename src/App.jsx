d4233037cfcd9a12cc539b3c6ca0c8c6408d2ba6
Milanych <vadimzotov.uk@gmail.com>
2023-10-18 11:28:25 +0100

    added navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React, {useState} from "react";
+import Nav from './Nav.jsx';
 import {ArtistHeader, AlbumsSubscribe, AlbumsList} from "./components/";
 import './css/App.css';
+import { Routes, Route } from "react-router-dom";
+
 
 export default function App() {
   return (
     <>
-      <ArtistHeader/>
-      <AlbumsSubscribe/>
-      <AlbumsList/>
+    <Routes>
+      <Route path="/" element={<Nav />}>
+        <Route index element={<ArtistHeader/>}/>
+          <Route path="subscribe" element={<AlbumsSubscribe/>}/>
+          <Route path="albums" element={<AlbumsList/>}/>
+       
+        <Route path="*" element={<h1 className='mt-4'>Page Not Found</h1>} />
+      </Route>
+    </Routes>
     </>
   )
 }
--- src/components/AlbumsList/AlbumsList.jsx
+++ src/components/AlbumsList/AlbumsList.jsx
@@ -5,8 +5,9 @@ import secondAlbum from '../../img/2.jpg'
 import thirdAlbum from '../../img/3.jpg'
 import LikeButton from "../LikeButton/LikeButton";
 import AlbumItem from "../AlbumItem/AlbumItem";
-
+import { useNavigate } from "react-router-dom";
 export default function Albums() {
+  const navigate = useNavigate();
 
   const albums = [
     {name: 'Fantasy', cover: firstAlbum, year: '1989'},
@@ -15,16 +16,18 @@ export default function Albums() {
   ]
 
   return (
-    <>
-    <h2>Albums list</h2>
+    <div className="mt-5 text-center">
+    <button className="btn btn-info mb-4" onClick={() => navigate('/')}>← Back to main</button>
+
+    <h2 className="mb-4">Albums list</h2>
     <Carousel indicators={false} className="carouselCentered">
-      {albums.map((album) => (
+      {albums.map((album, i) => (
       <Carousel.Item>
         <AlbumItem album={album} />
         <LikeButton/>
       </Carousel.Item>
       ))}
     </Carousel>
-    </>
+    </div>
   );
 }
